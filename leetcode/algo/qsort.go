12c931570bb2400469d1e668358975ef7fd04c7e
shiftt <zhong@brae.co>
2020-02-05 15:58:12 +0800

    切片为引用类型，同属引用类型的还有字典类型、通道类型、函数类型等

--- leetcode/algo/qsort.go
+++ leetcode/algo/qsort.go
@@ -4,35 +4,35 @@ import "fmt"
 
 func main() {
 	nums := []int{1, 3, 4, 1, 6, 6, 9}
-	QSort(&nums)
+	QSort(nums)
 	fmt.Println(nums)
 }
 
-func QSort(nums *[]int) {
-	qSort(nums, 0, len(*nums)-1)
+func QSort(nums []int) {
+	qSort(nums, 0, len(nums)-1)
 }
 
-func qSort(nums *[]int, left, right int) {
+func qSort(nums []int, left, right int) {
 	if left < right {
 		l, r := left, right
-		v := (*nums)[l]
+		v := nums[l]
 		for l < r {
-			for l < r && (*nums)[r] > v {
+			for l < r && nums[r] > v {
 				r--
 			}
 			if l < r {
-				(*nums)[l] = (*nums)[r]
+				nums[l] = nums[r]
 				l++
 			}
-			for l < r && (*nums)[l] < v {
+			for l < r && nums[l] < v {
 				l++
 			}
 			if l < r {
-				(*nums)[r] = (*nums)[l]
+				nums[r] = nums[l]
 				r--
 			}
 		}
-		(*nums)[l] = v
+		nums[l] = v
 		qSort(nums, left, l-1)
 		qSort(nums, l+1, right)
 	}
